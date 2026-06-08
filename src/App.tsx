85ae3cebe65e633a69a337b4775d4da8043f5f2e
fevico <ajayifavour81@gmail.com>
2026-06-08 17:10:53 +0100

    initial commit for client voting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-500">
+        <Button variant="default">Hello</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
